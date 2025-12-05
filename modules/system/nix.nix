60bbda12dae4585dae51bc17c627a09ed4ddf627
Aliaksandr <grubian2@gmail.com>
2025-12-05 04:28:01 +0200

    nix: add ekala corepkgs cachix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -65,18 +65,20 @@ in
         "https://cache.nixos.org?priority=1" # lower number means higher priority
         "https://nixos-cache-proxy.cofob.dev" # mirror
         "https://nix-community.cachix.org" # cache for unfree packages
-        "https://hyprland.cachix.org"
-        "https://cache.garnix.io"
-        "https://chaotic-nyx.cachix.org"
-        "https://nix-gaming.cachix.org"
+        "https://cache.garnix.io" # some more community packages
+        "https://chaotic-nyx.cachix.org" # chaotic nyx packages
+        "https://ekala-corepkgs.cachix.org" # corepkgs
+        "https://nix-gaming.cachix.org" # some gaming packages
+        "https://hyprland.cachix.org" # hyprland git packages
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "ekala-corepkgs.cachix.org-1:DcZV+vegWoEzacbSdXFXU4S7728C0eS9RfGpKeyHd6w="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
