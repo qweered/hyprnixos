d6f7afc16aedd70ecac2de6dca9d7d254375cf31
Aliaksandr <grubian2@gmail.com>
2025-12-08 22:09:48 +0200

    dank-materia: flake -> nixos module

---
+++ modules/system/desktop-environtments/dank-material.nix
@@ -0,0 +1,13 @@
+{
+  programs.dms-shell = {
+    enable = true;
+    enableVPN = false;
+  };
+
+  # TODO: requires config for keyboard etc.
+  # services.displayManager.dms-greeter = {
+  #   enable = true;
+  #   configHome = "/home/qweered";
+  #   compositor.name = "hyprland";
+  # };
+}
