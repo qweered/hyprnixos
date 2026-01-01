fe846e61ada96f1ae63e6da08631e0e47e4e27c2
Aliaksandr <grubian2@gmail.com>
2026-01-01 19:12:51 +0200

    flake: deduplicate lockfile

--- flake.nix
+++ flake.nix
@@ -7,15 +7,66 @@
     # nixpkgs-local-testing.url = "git+file:///home/qweered/Projects/nixpkgs";
     # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/master";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    nfh.url = "github:name-snrl/nfh";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    # global inputs, other will follow them
+    lib.url = "github:nix-community/nixpkgs.lib";
+    systems.url = "github:nix-systems/default";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "lib";
+    };
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
+    };
 
+    # other inputs
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nfh = {
+      url = "github:name-snrl/nfh";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.gitignore.follows = "gitignore";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixcord = {
+      url = "github:kaylorben/nixcord";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+    cpu-microcodes = {
+      url = "github:platomav/CPUMicrocodes";
+      flake = false;
+    };
+    ucodenix = {
+      url = "github:e-tho/ucodenix";
+      inputs.cpu-microcodes.follows = "cpu-microcodes";
+    };
     declarative-flatpak.url = "github:in-a-dil-emma/declarative-flatpak/latest";
 
     # stylix = {
@@ -27,18 +78,6 @@
     #   url = "github:matadaniel/LazyVim-module";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
-
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    ucodenix.url = "github:e-tho/ucodenix";
-    nixcord.url = "github:kaylorben/nixcord";
-    spicetify = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     # TODO: move into my repo
     easyeffects-presets = {
