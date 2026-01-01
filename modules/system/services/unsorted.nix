7f5b0fa17b1e2465c6bef155f3b0e3c1b3845ed3
Aliaksandr <grubian2@gmail.com>
2026-01-01 17:19:38 +0200

    devmon: nuke
    it is unmaintained and i alreade have udisks2

--- modules/system/services/unsorted.nix
+++ modules/system/services/unsorted.nix
@@ -5,7 +5,6 @@
 
   services.upower.enable = true; # power daemon, used by eg. chromium
   services.power-profiles-daemon.enable = true; # power daemon, for laptops mainly
-  services.devmon.enable = true; # device mounting daemon
   services.udisks2.enable = true; # disk storage daemon
   services.gvfs.enable = true; # mount, trash, etc in file managers
   services.tumbler.enable = true; # thumbnails in file managers
