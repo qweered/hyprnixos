f1c7d25c2aba888e51202c303f8829e1b75db118
Aliaksandr <grubian2@gmail.com>
2026-01-23 02:15:21 +0200

    office: onlyoffice -> libreoffice

--- modules/home/programs/media-files/office.nix
+++ modules/home/programs/media-files/office.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 {
-  # over libreoffice-qt-fresh (adds kde bloat), wpsoffice (broken)
-  home.packages = [ pkgs.onlyoffice-desktopeditors ];
+  # over libreoffice-qt-fresh (adds kde bloat), wpsoffice (broken), onlyoffice-desktopeditors (less features)
+  home.packages = [ pkgs.libreoffice-qt-fresh ];
 }
