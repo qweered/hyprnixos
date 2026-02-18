c613e84eb1611c50d721bfafa6613544c630a324
Aliaksandr <grubian2@gmail.com>
2026-02-19 01:36:11 +0200

    system: split overlays into separate file

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -32,20 +32,6 @@ in
     libraries = with pkgs; [ oxlint ]; # TODO: may be unneeded now
   };
 
-  # TODO: why overlays don't work in flake-parts?
-  nixpkgs.overlays = [
-    (_final: prev: {
-      nix-output-monitor = prev.nix-output-monitor.overrideAttrs (_old: {
-        src = prev.fetchFromGitHub {
-          owner = "maralorn";
-          repo = "nix-output-monitor";
-          rev = "4c34e115ab344df485316d4a61768b8d561fbeb3";
-          hash = "sha256-CcdGDNLkCsncYI+S5O71YgxQm2XLD8zPiDQQIebEdJ0=";
-        };
-      });
-    })
-  ];
-
   nix = {
     channel.enable = false;
 
