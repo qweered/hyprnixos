56615caa5fb6f42c420e9d0bdf80be408c892a30
Aliaksandr <grubian2@gmail.com>
2026-02-19 01:30:16 +0200

    vicinae: small cleanup

--- modules/home/programs/vicinae.nix
+++ modules/home/programs/vicinae.nix
@@ -1,16 +1,14 @@
 { inputs, pkgs, ... }:
 {
   imports = [ inputs.vicinae.homeManagerModules.default ];
-  disabledModules = [ "programs/vicinae.nix" ];
+  disabledModules = [ "programs/vicinae.nix" ]; # to be sure that we don't use it
 
   services.vicinae = {
     enable = true;
     package = pkgs.vicinae;
     systemd.enable = true;
-
     settings = {
       close_on_focus_loss = true;
-
       providers = {
         "@Gelei/bluetooth-0" = {
           preferences = {
