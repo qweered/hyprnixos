e77179bcdf81036693dceaa20dfee12cd49735de
Aliaksandr <grubian2@gmail.com>
2026-03-02 01:52:57 +0200

    sudo: split from unsorted, switch to sudo-rs

--- modules/system/unsorted.nix
+++ modules/system/unsorted.nix
@@ -4,12 +4,4 @@
 
   services.upower.enable = true; # power daemon, used by eg. chromium
   services.power-profiles-daemon.enable = true; # power daemon, for laptops mainly
-
-  # Only allow members of the wheel group to execute sudo by setting the executable’s permissions accordingly
-  # This prevents users that are not members of wheel from exploiting vulnerabilities in sudo such as CVE-2021-3156
-  security.sudo.execWheelOnly = true;
-  # Don't lecture the user. Less mutable state.
-  security.sudo.extraConfig = ''
-    Defaults lecture = never
-  '';
 }
