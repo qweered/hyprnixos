191493a8f7ab92eacd02c1fb3fe851be04ce5beb
Aliaksandr <grubian2@gmail.com>
2026-03-05 01:57:10 +0200

    coderabbit: init

--- modules/system/overlays.nix
+++ modules/system/overlays.nix
@@ -22,7 +22,7 @@ in
       });
       nurl = prev.nurl.override { nix = config.nix.package; };
       nix-init = prev.nix-init.override { nix = config.nix.package; };
-      inherit (llm-agents) opencode claude-code;
+      inherit (llm-agents) opencode claude-code coderabbit-cli;
       amp-cli = llm-agents.amp;
     })
   ];
