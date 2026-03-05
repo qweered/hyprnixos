59d70aa76921cbfb7684bdf3d90845c8d299427c
Aliaksandr <grubian2@gmail.com>
2026-03-05 04:19:19 +0200

    starship: add note about jj-starship

--- modules/home/programs/terminal/starship.nix
+++ modules/home/programs/terminal/starship.nix
@@ -1,4 +1,21 @@
 {
+  # NOTE: very slow performance in nixpkgs: https://github.com/dmmulroy/jj-starship/issues/17
+  # home.packages = [ pkgs.jj-starship ];
+  # programs.starship.settings = {
+  #   custom.jj = {
+  #     when = "jj-starship detect";
+  #     shell = [ "jj-starship" ];
+  #     format = "$output ";
+  #     ignore_timeout = true;
+  #   };
+  #   git_branch = {
+  #     disabled = true;
+  #   };
+  #   git_status = {
+  #     disabled = true;
+  #   };
+  # };
+
   # CONFIG
   programs.starship.enable = true;
 }
