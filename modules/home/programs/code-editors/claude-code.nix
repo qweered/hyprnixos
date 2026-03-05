d1fe6e2819cc5a335ce981ef45f461bcaa5525d8
Aliaksandr <grubian2@gmail.com>
2026-03-05 03:04:29 +0200

    claude-code: use mcp, add plugins

--- modules/home/programs/code-editors/claude-code.nix
+++ modules/home/programs/code-editors/claude-code.nix
@@ -1,8 +1,19 @@
 {
   programs.claude-code = {
     enable = true;
+    enableMcpIntegration = true;
     settings = {
       outputStyle = "Explanatory";
+      enabledPlugins = {
+        "coderabbit@claude-plugins-official" = true;
+        "frontend-design@claude-plugins-official" = true;
+        # "github@claude-plugins-official" = true;
+        "feature-dev@claude-plugins-official" = true;
+        "code-simplifier@claude-plugins-official" = true;
+        "typescript-lsp@claude-plugins-official" = true;
+        "commit-commands@claude-plugins-official" = true;
+        "security-guidance@claude-plugins-official" = true;
+      };
       attribution = {
         commit = "";
         pr = "";
