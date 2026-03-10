40e0c8b807cd98441ed2c69b423cdabc51c168a3
Aliaksandr <grubian2@gmail.com>
2026-03-10 17:21:12 +0200

    system: busybox, more uutils replacements

--- modules/system/debloat-modernize.nix
+++ modules/system/debloat-modernize.nix
@@ -26,9 +26,47 @@
   environment = {
     defaultPackages = lib.mkForce [ ];
     systemPackages = with pkgs; [
+      # TODO: replace ancient utils systemwide in nixpkgs
       uutils-coreutils-noprefix
       uutils-findutils
       uutils-diffutils
+      uutils-util-linux
+      uutils-login
+      uutils-sed
+      uutils-tar
+      uutils-hostname
+      uutils-procps
+      (lib.lowPrio inetutils) # for ftp
+      (lib.lowPrio (
+        busybox.override {
+          # Prevent busybox from overriding system binaries (systemd, util-linux, shadow, etc.)
+          extraConfig = lib.concatMapStrings (opt: "CONFIG_${opt} n\n") [
+            "HALT"
+            "POWEROFF"
+            "REBOOT"
+            "INIT"
+            "LOGIN"
+            "SU"
+            "SULOGIN"
+            "GETTY"
+            "PASSWD"
+            "VLOCK"
+            "MOUNT"
+            "UMOUNT"
+            "DMESG"
+            "KILL"
+            "KILLALL"
+            "SYSLOGD"
+            "KLOGD"
+            "FSCK"
+            "MKSWAP"
+            "SWAPON"
+            "SWAPOFF"
+            "PIVOT_ROOT"
+            "SWITCH_ROOT"
+          ];
+        }
+      )) # various utils
     ];
   };
 
