0646f33a4de5e2ca208d7db88fa7cfdfb27fe65e
Aliaksandr <grubian2@gmail.com>
2026-03-12 05:21:09 +0200

    mcp: add exa

--- modules/home/programs/code-editors/mcp.nix
+++ modules/home/programs/code-editors/mcp.nix
@@ -1,12 +1,18 @@
 {
   programs.mcp = {
     enable = true;
-    servers.context7 = {
-      command = "npx";
-      args = [
-        "-y"
-        "@upstash/context7-mcp"
-      ];
+    servers = {
+      context7 = {
+        command = "npx";
+        args = [
+          "-y"
+          "@upstash/context7-mcp"
+        ];
+      };
+      exa = {
+        mode = "http";
+        url = "https://mcp.exa.ai/mcp";
+      };
     };
   };
 }
