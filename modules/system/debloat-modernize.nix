c8fb329192084fa56b4e0e7bd8897048ac5b0676
Aliaksandr <grubian2@gmail.com>
2026-04-12 22:38:46 +0300

    preSwitchCheck: move to security

--- modules/system/debloat-modernize.nix
+++ modules/system/debloat-modernize.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, ... }:
 
 {
   # Perlless https://github.com/NixOS/nixpkgs/blob/0260f927b7c1578b5c7cdefd7db7b660565cd362/nixos/modules/profiles/perlless.nix
@@ -30,46 +30,4 @@
 
   systemd.enableStrictShellChecks = true; # CONFIG: will become default
   services.dbus.implementation = "broker"; # CONFIG: will become default
-
-  # Protection against changing hostname between deploys
-  # Stolen from https://github.com/nix-community/srvos/blob/c4a21c42efec0506ec352891fec84490dae2ded0/nixos/common/detect-hostname-change.nix
-  system.preSwitchChecks.detectHostnameChange = lib.mkIf (config.networking.hostName != "") ''
-    detectHostnameChange() {
-      local actual
-      actual=$(< /proc/sys/kernel/hostname)
-
-      # Ignore if the system is getting installed
-      # https://github.com/nix-community/nixos-images/blob/2fc023e024c0a5e8e98ae94363dbf2962da10886/nix/installer.nix#L12-L13
-      if [[ ! -e /run/booted-system || "$actual" == "nixos-installer" ]]; then
-        return
-      fi
-
-      desired=${config.networking.hostName}
-
-      if [[ "$actual" = "$desired" ]]; then
-        return
-      fi
-
-      # Useful for automation
-      if [[ "''${EXPECTED_HOSTNAME:-}" = "$desired" ]]; then
-        return
-      fi
-
-      log() {
-        echo "$*" >&2
-      }
-
-      log "WARNING: machine hostname change detected from '$actual' to '$desired'"
-      log
-      log "Are you deploying on the right host?"
-      log
-      log "Type YES to continue:"
-      read -r reply
-      if [[ $reply != YES ]]; then
-        echo "aborting"
-        exit 1
-      fi
-    }
-    detectHostnameChange
-  '';
 }
