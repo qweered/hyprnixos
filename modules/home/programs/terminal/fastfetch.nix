102c8b8b24fed10c04d195f70808ddbd3a6d3696
Aliaksandr <grubian2@gmail.com>
2026-04-24 18:53:53 +0300

    fastfetch: speedup a little

--- modules/home/programs/terminal/fastfetch.nix
+++ modules/home/programs/terminal/fastfetch.nix
@@ -1,4 +1,30 @@
 {
   # CONFIG
-  programs.fastfetch.enable = true;
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages" # add 100ms to fetch time
+        "shell"
+        "wm" # add 100ms to fetch time
+        "theme"
+        "icons"
+        "font"
+        "monitor"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "disk"
+        "break"
+        "colors"
+      ];
+    };
+  };
 }
