2082104694d5bd835336ec2d63a92dda6466777a
Aliaksandr <grubian2@gmail.com>
2026-05-24 15:07:43 +0300

    busybox: disable more utils covered by other packages

--- modules/system/programs/utils.nix
+++ modules/system/programs/utils.nix
@@ -1,5 +1,46 @@
 { pkgs, lib, ... }:
 
+let
+  busyboxDisabledApplets = [
+    # inetutils owns these commands in the system profile.
+    "DNSDOMAINNAME"
+    "IFCONFIG"
+    "LOGGER"
+    "PING"
+    "PING6"
+    "TELNET"
+    "TFTP"
+    "TRACEROUTE"
+    "WHOIS"
+
+    # Core system packages own these commands.
+    "DMESG" # util-linux
+    "FSCK" # util-linux
+    "GETTY" # util-linux
+    "HALT" # systemd
+    "INIT" # systemd
+    "KILL" # util-linux
+    "KILLALL" # procps
+    "LOGIN" # shadow
+    "MKSWAP" # util-linux
+    "MOUNT" # util-linux
+    "PASSWD" # shadow
+    "PIVOT_ROOT" # util-linux
+    "POWEROFF" # systemd
+    "REBOOT" # systemd
+    "SU" # shadow
+    "SULOGIN" # util-linux
+    "SWAPOFF" # util-linux
+    "SWAPON" # util-linux
+    "SWITCH_ROOT" # util-linux
+    "UMOUNT" # util-linux
+    "VLOCK" # shadow
+
+    # Not provided by busybox in this system profile.
+    "KLOGD"
+    "SYSLOGD"
+  ];
+in
 {
   environment.systemPackages = with pkgs; [
     # TODO: replace ancient utils systemwide in nixpkgs
@@ -19,32 +60,8 @@
     # various utils
     (lib.lowPrio (
       busybox.override {
-        # Prevent busybox from overriding system binaries (systemd, util-linux, shadow, etc.)
-        extraConfig = lib.concatMapStrings (opt: "CONFIG_${opt} n\n") [
-          "HALT"
-          "POWEROFF"
-          "REBOOT"
-          "INIT"
-          "LOGIN"
-          "SU"
-          "SULOGIN"
-          "GETTY"
-          "PASSWD"
-          "VLOCK"
-          "MOUNT"
-          "UMOUNT"
-          "DMESG"
-          "KILL"
-          "KILLALL"
-          "SYSLOGD"
-          "KLOGD"
-          "FSCK"
-          "MKSWAP"
-          "SWAPON"
-          "SWAPOFF"
-          "PIVOT_ROOT"
-          "SWITCH_ROOT"
-        ];
+        # Prevent busybox from exporting applets already owned by full packages.
+        extraConfig = lib.concatMapStrings (opt: "CONFIG_${opt} n\n") busyboxDisabledApplets;
       }
     ))
   ];
