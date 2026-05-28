476919182d8c253919819752b6cb41d65e4f6d53
Aliaksandr <grubian2@gmail.com>
2026-05-28 21:15:06 +0300

    scx: disable, does not work for cachy rt kernel

--- modules/system/services/scx.nix
+++ modules/system/services/scx.nix
@@ -2,8 +2,11 @@
 
 {
   # sched-ext schedulers
+  # Disabled: the rt-bore (PREEMPT_RT) kernel cannot load scx schedulers
+  # (BPF struct_ops attach fails with EOPNOTSUPP).
+  # See https://github.com/xddxdd/nix-cachyos-kernel/issues/83
   services.scx = {
-    enable = true;
+    enable = false;
     package = pkgs.scx.rustscheds;
     # Lest use default for now understand what is better for my system
     # scheduler = "scx_lavd";
