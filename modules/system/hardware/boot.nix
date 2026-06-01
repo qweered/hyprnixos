4cf5786071862195b39ece40ba5c185f433dbc3d
Aliaksandr <grubian2@gmail.com>
2026-06-02 01:28:10 +0300

    boot: initrd is default now

--- modules/system/hardware/boot.nix
+++ modules/system/hardware/boot.nix
@@ -12,9 +12,6 @@
     };
     tmp.cleanOnBoot = true;
 
-    # NOTE: Not feature-complete yet https://blog.decent.id/post/nixos-systemd-initrd/
-    initrd.systemd.enable = true;
-
     kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-rt-bore-lto; # or latest-lto-x86_64-v3;
 
     plymouth = {
