b7c03755b1fb16421cb066feca12eac8f3633ce6
Aliaksandr <grubian2@gmail.com>
2026-06-01 16:31:02 +0300

    scx: enable only on non-rt kernels

--- modules/system/services/scx.nix
+++ modules/system/services/scx.nix
@@ -1,12 +1,16 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 {
-  # sched-ext schedulers
-  # Disabled: the rt-bore (PREEMPT_RT) kernel cannot load scx schedulers
-  # (BPF struct_ops attach fails with EOPNOTSUPP).
+  # Enabled only on non-rt kernels: the rt-bore (PREEMPT_RT) kernel cannot load
+  # scx schedulers (BPF struct_ops attach fails with EOPNOTSUPP).
   # See https://github.com/xddxdd/nix-cachyos-kernel/issues/83
   services.scx = {
-    enable = false;
+    enable = !lib.hasInfix "-rt" config.boot.kernelPackages.kernel.name;
     package = pkgs.scx.rustscheds;
     # Lest use default for now understand what is better for my system
     # scheduler = "scx_lavd";
