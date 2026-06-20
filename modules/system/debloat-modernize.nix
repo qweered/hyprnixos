91963bd160f1744fdcbc511dd2da7a2bae5f527f
Aliaksandr <grubian2@gmail.com>
2026-06-20 04:21:46 +0300

    debloat-modernize: dbus broker default now, installerTools only 800kb

--- modules/system/debloat-modernize.nix
+++ modules/system/debloat-modernize.nix
@@ -2,8 +2,8 @@
 
 {
   # Perlless https://github.com/NixOS/nixpkgs/blob/0260f927b7c1578b5c7cdefd7db7b660565cd362/nixos/modules/profiles/perlless.nix
-  system.disableInstallerTools = true; # remove generate, install, enter, option, version, build-vms, firewall
-  system.tools.nixos-rebuild.enable = true; # but keep rebuild
+  # TODO: Check that the system does not contain a Nix store path that contains the string "perl".
+  # system.forbiddenDependenciesRegexes = [ "perl" ];
 
   # Remove unnecessary packages
   environment.defaultPackages = lib.mkForce [ ];
@@ -16,14 +16,10 @@
     nixos.enable = false;
   };
 
-  # TODO: Check that the system does not contain a Nix store path that contains the string "perl".
-  # system.forbiddenDependenciesRegexes = [ "perl" ];
-
   # If /etc is read-only, we need to provide the machine-id file as a mount point for systemd.
   # https://www.freedesktop.org/software/systemd/man/256/machine-id.html#Initialization
   # environment.etc."machine-id".text = "";
   # system.etc.overlay.enable = true; # multiple errors for now
 
   systemd.enableStrictShellChecks = true; # CONFIG: will become default
-  services.dbus.implementation = "broker"; # CONFIG: will become default
 }
