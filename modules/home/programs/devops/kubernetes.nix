20aa66477e20e5c2d33167dba4dc53aad883d8b9
Aliaksandr <grubian2@gmail.com>
2026-07-09 22:57:01 +0300

    kubernetes, talos: init

---
+++ modules/home/programs/devops/kubernetes.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    kubectl
+    krew
+  ];
+
+  # krew install radar
+
+  programs.fish.interactiveShellInit = "set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin";
+}
