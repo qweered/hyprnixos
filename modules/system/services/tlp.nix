207ac07a310f820989fd20a2a48574a23e8e3e81
Aliaksandr <grubian2@gmail.com>
2026-08-09 17:08:46 +0300

    tlp: fixup slightly

--- modules/system/services/tlp.nix
+++ modules/system/services/tlp.nix
@@ -1,9 +1,21 @@
+{ config, pkgs, ... }:
+let
+  tlp = pkgs.tlp.override {
+    enableRDW = config.networking.networkmanager.enable;
+    # TODO: upstream
+    systemd = config.systemd.package;
+  };
+in
 {
   # power management daemon with battery optimization (over power-profiles-daemon, auto-cpufreq)
   services = {
     tlp = {
+      package = tlp;
       enable = true;
-      pd.enable = true;
+      pd = {
+        enable = true;
+        package = pkgs.tlp-pd.override { inherit tlp; };
+      };
       settings = {
         WIFI_PWR_ON_BAT = "on";
         PCIE_ASPM_ON_BAT = "powersupersave";
