135b7315ebba1fdf3e15780ecd5f80ae7b758537
Aliaksandr <grubian2@gmail.com>
2026-08-13 09:26:08 +0300

    utils: enable more utils

--- modules/system/programs/utils.nix
+++ modules/system/programs/utils.nix
@@ -1,33 +1,19 @@
 { pkgs, lib, ... }:
-
 let
-  # Applets are plain on/off toggles, all enabled (y). SH_IS_* is different: it
-  # is a kconfig `choice`, not an applet. bash owns sh in this profile, so we
-  # pick `none` and must also clear the other arms, else `make oldconfig`
-  # re-prompts for the choice and the build hangs.
   busyboxConfig = {
-    IPCALC = "y";
-    MICROCOM = "y";
-
-    I2CDETECT = "y";
-    I2CDUMP = "y";
-    I2CGET = "y";
-    I2CSET = "y";
-    I2CTRANSFER = "y";
-
-    LSPCI = "y";
-    LSSCSI = "y";
-    LSUSB = "y";
-
     ASCII = "y";
     BC = "y";
+    DEVMEM = "y";
     DOS2UNIX = "y";
     HEXEDIT = "y";
+    KILLALL = "y";
     LZOP = "y";
+    MICROCOM = "y";
     NMETER = "y";
     TTYSIZE = "y";
     UNIX2DOS = "y";
 
+    # To not reprompt `make oldconfig`
     SH_IS_ASH = "n";
     SH_IS_HUSH = "n";
     SH_IS_NONE = "y";
@@ -35,7 +21,6 @@ let
 in
 {
   environment.systemPackages = with pkgs; [
-    # TODO: replace ancient utils systemwide in nixpkgs
     uutils-coreutils-noprefix
     # TODO: enable once stable enough
     #uutils-diffutils
@@ -46,10 +31,11 @@ in
     #uutils-tar
     #uutils-hostname
     #uutils-procps
-    # for ftp
-    (lib.lowPrio inetutils)
+    usbutils # lsusb
+    pciutils # lspci, setpci
     iw
-    # various utils
+    lsof
+    (lib.lowPrio inetutils) # for ftp and telnet
     (lib.lowPrio (
       busybox.override {
         enableMinimal = true;
