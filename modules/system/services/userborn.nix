83450f108a204caa42b59a08cf5769fa8ee1ca66
Aliaksandr <grubian2@gmail.com>
2026-08-18 02:06:59 +0300

    userborn: leave a note

--- modules/system/services/userborn.nix
+++ modules/system/services/userborn.nix
@@ -1,4 +1,7 @@
 {
   # declarative user management
   services.userborn.enable = true;
+
+  # TODO: uncomment after https://github.com/NixOS/nixpkgs/pull/508608
+  # services.userborn.importLegacyState = false;
 }
